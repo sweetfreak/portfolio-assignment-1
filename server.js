f27a60041d0c71cfb67438ecf21001dab7bf8f16
jesse sheehan <jessetsheehan@gmail.com>
2022-07-31 18:35:44 -0400

    another heroku change

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-const path = require("path");
 const express = require("express");
 const session = require("express-session");
-const exphbs = require("express-handlebars");
-const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,16 +19,10 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(require("./controllers/"));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
